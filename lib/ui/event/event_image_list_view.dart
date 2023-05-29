c8a2ded3881eeb1abf499c962010c6fb7a78cb2e
Kevin Runion <kevinx@gmail.com>
2023-05-29 16:00:09 -0500

    loading shimmer constructor

--- lib/ui/event/event_image_list_view.dart
+++ lib/ui/event/event_image_list_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:indegoal/lib.dart';
-import 'package:shimmer/shimmer.dart';
 
 class EventImageListView extends ConsumerWidget {
   const EventImageListView({
@@ -16,11 +15,7 @@ class EventImageListView extends ConsumerWidget {
     return ref
         .watch(eventImagesProvider(eventId: eventId, width: 200, height: 200))
         .when(
-          loading: () => Shimmer.fromColors(
-            baseColor: Colors.grey.shade300,
-            highlightColor: Colors.grey.shade100,
-            child: const Card(child: SizedBox.expand()),
-          ),
+          loading: () => const Loading.shimmer(),
           error: (error, stackTrace) => ErrWidget(error),
           data: (images) => Card(
             child: GridView.count(
--- lib/widgets/loading.dart
+++ lib/widgets/loading.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class Loading extends StatelessWidget {
-  const Loading({super.key});
+  const Loading({super.key, this.child, this.baseColor, this.highlightColor})
+      : shimmer = false;
+  const Loading.shimmer({
+    super.key,
+    this.child,
+    this.baseColor,
+    this.highlightColor,
+  }) : shimmer = true;
 
+  final Widget? child;
+  final bool shimmer;
+  final Color? baseColor, highlightColor;
   @override
   Widget build(BuildContext context) {
-    return const SizedBox.shrink();
+    return switch (shimmer) {
+      false => child ?? const SizedBox.shrink(),
+      true => Shimmer.fromColors(
+          baseColor: baseColor ?? Colors.grey.shade300,
+          highlightColor: highlightColor ?? Colors.grey.shade100,
+          child: child ?? const Card(child: SizedBox.expand())),
+    };
   }
 }
